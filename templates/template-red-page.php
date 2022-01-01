0e613b360a3a32aa041c06d7ad0b52f65f8eb031
Kees van Ginkel <kees@vanginkels.com>
2022-01-01 17:26:41 +0100

    extra templates

---
+++ templates/template-red-page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Red Page Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+get_template_part( 'singular' );
